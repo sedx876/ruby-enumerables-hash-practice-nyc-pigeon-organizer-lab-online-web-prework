c2f72d3dfd3a3031ad2b43268bdc7cf78c776bf2
Sharon Watkins <swatkins876@gmail.com>
2019-08-13 13:44:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon_data.each do |attribute, attribute_data|
+    attribute_data.each do |option, pigeon_array|
+      pigeon_array.each do |pigeon|
+        if pigeon_names.include?(pigeon) == false
+          pigeon_names << pigeon
+          pigeon_list[pigeon] = {:color => [], :gender => [], :lives => []}
+        end
+      end
+    end
+  end
 end
+
+pigeon_names.each do |pigeon_name|
+    pigeon_data.each do |attribute, attribute_data|
+      array = []
+      attribute_data.each do |option, pigeon_array|
+        if pigeon_array.include?(pigeon_name)
+          array << option.to_s
+        end
+      end
+      pigeon_list[pigeon_name][attribute] = array
+    end
+  end
+pigeon_list
+end
+
